10083be01ba5aab240add7f23d981511c3c2124d
0xKurt <Kurt@Merbeth.io>
2024-07-11 22:19:20 +0200

    update structure, logo, css

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,17 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
