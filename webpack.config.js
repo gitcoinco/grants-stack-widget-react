88b4c307523726dbd95795184e68553671c8c5a5
0xKurt <Kurt@Merbeth.io>
2024-07-11 19:37:37 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'MyComponentLibrary',
-    libraryTarget: 'umd',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "Widget",
+    libraryTarget: "umd",
   },
   externals: {
-    react: 'react',
-    'react-dom': 'react-dom'
-  }
+    react: "react",
+    "react-dom": "react-dom",
+  },
 };
